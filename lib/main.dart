d1ecfad5069f569e976ef2dcadb2e248ffc7a0d6
adiKK17 <you@example.com>
2019-08-30 00:27:45 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyFoodApp());
+}
+
+class MyFoodApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyFoodApp();
+  }
+}
+
+class _MyFoodApp extends State<MyFoodApp> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+//      home: HomePage(),
+    );
+  }
+}
