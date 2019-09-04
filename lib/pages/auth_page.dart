dd02a5a130dcfa1d482006b57e4d08c226a6ffde
adiKK17 <you@example.com>
2019-09-04 23:59:54 +0530

    added error handling and now authentication seems to be working with firebase

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 
 import 'home_page.dart';
 import '../providers/auth.dart';
+import '../http_exception.dart';
 
 enum AuthMode { Login, SignUp }
 
@@ -81,18 +82,34 @@ class _AuthenticationPage extends State<AuthenticationPage> {
 
   Widget _buildSubmitButton() {
     return RaisedButton(
-      color: Colors.black87,
-      child: Text(
-        _authMode == AuthMode.Login ? "LOGIN" : "SIGNUP",
-        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        color: Colors.black87,
+        child: Text(
+          _authMode == AuthMode.Login ? "LOGIN" : "SIGNUP",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        onPressed: () {
+          _submitForm();
+        });
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("An error Occured!"),
+        content: Text(message),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text("Okay"),
+          ),
+        ],
       ),
-      onPressed: () { _submitForm();
-        }
     );
   }
 
   Future<void> _submitForm() async {
-    if(!_formKey.currentState.validate()){
+    if (!_formKey.currentState.validate()) {
       return;
     }
     _formKey.currentState.save();
@@ -101,24 +118,43 @@ class _AuthenticationPage extends State<AuthenticationPage> {
       _isLoading = true;
     });
 
+    try {
 
-    if(_authMode == AuthMode.Login){
-     await Provider.of<Auth>(context,listen: false).login(_formData["email"], _formData["password"]);
+      if (_authMode == AuthMode.Login) {
+        await Provider.of<Auth>(context, listen: false)
+            .login(_formData["email"], _formData["password"]);
+      } else {
+        await Provider.of<Auth>(context, listen: false)
+            .signUp(_formData["email"], _formData["password"]);
+      }
 
-    }else{
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
 
-      await  Provider.of<Auth>(context,listen: false).signUp(_formData["email"], _formData["password"]);
+    } on HttpException catch (error) {
+      var errorMessage = "authentication Failed!";
+      if (error.toString().contains("EMAIL_EXISTS")) {
+        errorMessage = "This email address is already in use!";
+      } else if (error.toString().contains("INVALID_EMAIL")) {
+        errorMessage = "This is not a valid email!";
+      } else if (error.toString().contains("WEAK_PASSWORD")) {
+        errorMessage = "This password is too weak!";
+      } else if (error.toString().contains("EMAIL_NOT_FOUND")) {
+        errorMessage = "This Email does not exist!";
+      } else if (error.toString().contains("INVALID_PASSWORD")) {
+        errorMessage = "Invalid Password!";
+      }
+      _showErrorDialog(errorMessage);
+
+    } catch (error) {
+      var errorMessage = "Could not authenticate you! Try again later";
+      _showErrorDialog(errorMessage);
     }
-
 
     setState(() {
       _isLoading = false;
     });
-
-
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(builder: (context) => HomePage()),
-    );
 
 
   }
@@ -137,72 +173,74 @@ class _AuthenticationPage extends State<AuthenticationPage> {
         ),
         child: Form(
           key: _formKey,
-          child: SingleChildScrollView(child: Column(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(top: 220),
-                alignment: Alignment.center,
-                child: Text(
-                  "ReciPedia",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
-                      fontSize: 50),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(top: 220),
+                  alignment: Alignment.center,
+                  child: Text(
+                    "ReciPedia",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 50),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 70,
-              ),
-              Container(
-                child: _buildEmailTextField(),
-                width: deviceWidth * 0.85,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Container(
-                child: _buildPasswordTextField(),
-                width: deviceWidth * 0.85,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              _authMode == AuthMode.SignUp
-                  ? Container(
-                child: _buildPasswordConfirmTextField(),
-                width: deviceWidth * 0.85,
-              )
-                  : Container(),
-              SizedBox(
-                height: 25,
-              ),
-              _isLoading == true ? CircularProgressIndicator() : Container(
-                child: _buildSubmitButton(),
-                width: 200,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              FlatButton(
-                onPressed: () {
-                  setState(() {
-
-                    if (_authMode == AuthMode.Login) {
-                      _authMode = AuthMode.SignUp;
-                    } else {
-                      _authMode = AuthMode.Login;
-                    }
-
-                  });
-                },
-                child: Text(_authMode == AuthMode.Login
-                    ? "Not a user?  SignUp first"
-                    : "Login Instead"),
-              ),
-            ],
+                SizedBox(
+                  height: 70,
+                ),
+                Container(
+                  child: _buildEmailTextField(),
+                  width: deviceWidth * 0.85,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  child: _buildPasswordTextField(),
+                  width: deviceWidth * 0.85,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                _authMode == AuthMode.SignUp
+                    ? Container(
+                        child: _buildPasswordConfirmTextField(),
+                        width: deviceWidth * 0.85,
+                      )
+                    : Container(),
+                SizedBox(
+                  height: 25,
+                ),
+                _isLoading == true
+                    ? CircularProgressIndicator()
+                    : Container(
+                        child: _buildSubmitButton(),
+                        width: 200,
+                      ),
+                SizedBox(
+                  height: 30,
+                ),
+                FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      if (_authMode == AuthMode.Login) {
+                        _authMode = AuthMode.SignUp;
+                      } else {
+                        _authMode = AuthMode.Login;
+                      }
+                    });
+                  },
+                  child: Text(_authMode == AuthMode.Login
+                      ? "Not a user?  SignUp first"
+                      : "Login Instead"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
-    ),);
+    );
   }
 }
--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 
+import '../http_exception.dart';
+
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
@@ -12,26 +14,46 @@ class Auth with ChangeNotifier {
   Future<void> signUp(String email, String password) async {
     const url =
         "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAmJvi2hhrGVIV4mtj87XlVYqfAAjYP6V4";
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {"email": email, "password": password, "returnSecureToken": true},
-      ),
-    );
-    
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {"email": email, "password": password, "returnSecureToken": true},
+        ),
+      );
+
+      final responseData = json.decode(response.body);
+
+      if(responseData['error'] != null){
+          throw HttpException(responseData["error"]["message"]);
+      }
+    } catch(error) {
+      throw error;
+    }
+
   }
 
   Future<void> login(String email, String password) async {
     const url =
         "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAmJvi2hhrGVIV4mtj87XlVYqfAAjYP6V4";
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {"email": email, "password": password, "returnSecureToken": true},
-      ),
-    );
-    
+    try {
+
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {"email": email, "password": password, "returnSecureToken": true},
+        ),
+      );
+
+      final responseData = json.decode(response.body);
+
+      if(responseData['error'] != null){
+        throw HttpException(responseData["error"]["message"]);
+      }
+    } catch(error) {
+      throw error;
+    }
+  
   }
 
 
